78bff59aab1b4def9c0f142009e2c0ce9cf0db14
Yan Naing Aung <yannaingaung2209@gmail.com>
2025-11-15 19:30:13 +0630

    feat(TodoForm): add addTodo() to submit new todo

--- src/App.js
+++ src/App.js
@@ -5,14 +5,43 @@ import TodoList from "./components/TodoList";
 import CheckAllAndRemainingItem from "./components/CheckAllAndAndRemainingItem";
 import TodoFilter from "./components/TodoFilter";
 import ClearCompletedButton from "./components/ClearCompletedButton";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/todos")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTodos(data);
+      })
+      .catch((error) => console.log("Error: ", error));
+  }, []);
+
+  const addTodo = (todo) => {
+    // server
+    fetch("http://localhost:5000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    });
+    //client
+    setTodos((prevState) => [...prevState, todo]);
+  };
+
+  console.log(todos);
+
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
         {/* todo form */}
-        <TodoForm />
+        <TodoForm addTodo={addTodo} />
         {/* todo lists */}
         <TodoList />
         {/* check all and remaining items */}
--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import "./index.css";
 
-export default function TodoForm() {
+export default function TodoForm({ addTodo }) {
+  const [title, setTitle] = useState("");
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: Math.random(Math.floor() * 1000),
+      title,
+      completed: false,
+    };
+
+    addTodo(newTodo);
+    setTitle("");
+  };
+
   return (
-    <form action="#">
+    <form action="#" onSubmit={handleAddTodo}>
       <input
         type="text"
         className="todo-input"
         placeholder="What do you need to do?"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
     </form>
   );
